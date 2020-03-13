75fcb9bd9190e00c2ed025339e6136c977e0281c
goravjoshi <gaurav.joshi@live.com>
2020-03-13 11:40:47 +1100

    A working revoke button

--- src/main/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/token/OAuthTokenConfiguration.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/token/OAuthTokenConfiguration.java
@@ -1,6 +1,5 @@
 package com.atlassian.bitbucket.jenkins.internal.jenkins.oauth.token;
 
-import com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.consumer.Consumer;
 import com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.token.ServiceProviderTokenStore;
 import com.atlassian.bitbucket.jenkins.internal.jenkins.oauth.consumer.OAuthGlobalConfiguration;
 import hudson.Extension;
@@ -17,13 +16,9 @@ import javax.annotation.CheckForNull;
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import java.time.Clock;
-import java.util.Arrays;
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-
-import static com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.consumer.Consumer.SignatureMethod.HMAC_SHA1;
-import static com.atlassian.bitbucket.jenkins.internal.applink.oauth.serviceprovider.token.ServiceProviderToken.newRequestToken;
 
 @Extension
 public class OAuthTokenConfiguration implements Action, Describable<OAuthGlobalConfiguration> {
@@ -56,43 +51,16 @@ public class OAuthTokenConfiguration implements Action, Describable<OAuthGlobalC
 
     @SuppressWarnings("unused") // Stapler
     public List<DisplayAccessToken> getTokens() {
-        if (tokens == null) {
-            tokens = Arrays.asList(
-                    new DisplayAccessToken(
-                            newRequestToken("1")
-                                    .tokenSecret("123")
-                                    .creationTime(new Date().getTime())
-                                    .consumer(
-                                            Consumer.key("1")
-                                                    .name("cons")
-                                                    .consumerSecret("123")
-                                                    .signatureMethod(HMAC_SHA1)
-                                                    .build()).build()
-                                    .authorize("gj", "1"), clock),
-                    new DisplayAccessToken(newRequestToken("2").tokenSecret("123").creationTime(new Date().getTime())
-                            .consumer(Consumer.key("1").name("cons").consumerSecret("123").signatureMethod(HMAC_SHA1).build()).build().authorize("gj", "1"), clock),
-                    new DisplayAccessToken(newRequestToken("3").tokenSecret("123").creationTime(new Date().getTime())
-                            .consumer(Consumer.key("1").name("cons").consumerSecret("123").signatureMethod(HMAC_SHA1).build()).build().authorize("gj", "1"), clock),
-                    new DisplayAccessToken(newRequestToken("4").tokenSecret("123").creationTime(new Date().getTime())
-                            .consumer(Consumer.key("1").name("cons").consumerSecret("123").signatureMethod(HMAC_SHA1).build()).build().authorize("gj", "1"), clock),
-                    new DisplayAccessToken(newRequestToken("5").tokenSecret("123").creationTime(new Date().getTime())
-                            .consumer(Consumer.key("1").name("cons").consumerSecret("123").signatureMethod(HMAC_SHA1).build()).build().authorize("gj", "1"), clock),
-                    new DisplayAccessToken(newRequestToken("6").tokenSecret("123").creationTime(new Date().getTime())
-                            .consumer(Consumer.key("1").name("cons").consumerSecret("123").signatureMethod(HMAC_SHA1).build()).build().authorize("gj", "1"), clock)
-            );
-        }
-        return tokens;
+        List<DisplayAccessToken> tokenList = new ArrayList<>();
+        tokenStore.getAccessTokensForUser(Jenkins.getAuthentication().getName())
+                .forEach(token -> tokenList.add(new DisplayAccessToken(token, clock)));
 
-//        tokenStore.getAccessTokensForUser(Jenkins.getAuthentication().getName())
-//                .forEach(token -> tokenList.add(new DisplayAccessToken(token, clock)));
-//
-//        return tokenList;
+        return tokenList;
     }
 
     @RequirePOST
     public HttpResponse doRevoke(StaplerRequest request) throws ServletException {
-        request
-                .getParameterMap()
+        request.getParameterMap()
                 .entrySet()
                 .stream()
                 .filter(e -> e.getValue().length == 1 && e.getValue()[0].equals(REVOKE_BUTTON_NAME))
