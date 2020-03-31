56deaec98b96f0cb88750f87f2f22e4e228f6413
goravjoshi <gaurav.joshi@live.com>
2020-03-31 12:18:46 +1100

    Fixing test

--- src/test/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/servlet/AuthorizeConfirmationConfigTest.java
+++ src/test/java/com/atlassian/bitbucket/jenkins/internal/jenkins/oauth/servlet/AuthorizeConfirmationConfigTest.java
@@ -14,9 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.Clock;
-import java.util.Optional;
 
-import static net.oauth.OAuth.OAUTH_TOKEN;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -40,10 +38,6 @@ public class AuthorizeConfirmationConfigTest {
 
     @Test
     public void throwsExceptionForInvalidToken() throws FormException {
-        String aToken = "123";
-        when(request.getParameter(OAUTH_TOKEN)).thenReturn(aToken);
-        when(serviceProviderTokenStore.get(aToken)).thenReturn(Optional.empty());
-
         AuthorizeConfirmationConfigDescriptor descriptor = createDescriptor();
 
         expectedException.expect(FormException.class);
